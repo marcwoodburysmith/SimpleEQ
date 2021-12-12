120a65f6422fb1bef404bbb95af20811eaa117da
Marc Woodbury-Smith <marc.woodbury-smith@newcastle.ac.uk>
2021-12-12 13:23:45 +0000

    Part 10 Second bit to 1:59:20

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -28,12 +28,25 @@ highCutSlopeSliderAttachment(audioProcessor.apvts, "HighCut Slope", highCutSlope
        {
            addAndMakeVisible(comp);
        }
+    
+    const auto& params = audioProcessor.getParameters();
+    for (auto param: params)
+    {
+        param->addListener(this);
+    }
+    
+    startTimerHz(60);
        
     setSize (600, 400);
 }
 
 SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor()
 {
+    const auto& params = audioProcessor.getParameters();
+    for (auto param: params)
+    {
+        param->removeListener(this);
+    }
 }
 
 //==============================================================================
@@ -147,7 +160,11 @@ void SimpleEQAudioProcessorEditor::timerCallback()
     {
         //if parameters have changed need to:
         //update the monochain
+        auto chainSettings = getChainSettings(audioProcessor.apvts);
+        auto peakCoefficients = makePeakFilter(chainSettings, audioProcessor.getSampleRate());
+        updateCoefficients(monoChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
         //signal a repaint
+        repaint();
     }
 }
 
